ce905980e8eeee337e3ef81581e81db07a256ea2
Soliha <solyatursuntasheva@gmail.com>
2023-05-02 15:10:51 +0500

    Finish Header!

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Header from "../components/layout/header/Header";
 import { routesDatas } from "../mock/routes";
+import Header from "../components/layout/header/Header";
 
 const Rout = () => {
   return (
